4fc25551acdf6f1227ad70857d15c4d040bd3f2d
Thebroscable <57013863+Thebroscable@users.noreply.github.com>
2023-03-02 22:19:03 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,25 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import org.jsoup.nodes.Document;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Crawler> bots = new ArrayList<>();
-        BlockingQueue<String> linksQueue = new ArrayBlockingQueue<>(100);
+        BlockingQueue<Document> linksQueue = new ArrayBlockingQueue<>(10);
 
-        bots.add(new Crawler("https://coinmarketcap.com/", linksQueue));
+        Crawler crawler = new Crawler("https://www.coingecko.com/", linksQueue);
+        Scraper scraper = new Scraper(linksQueue, crawler);
 
-        for (Crawler crawler : bots) {
-            try {
-                crawler.getThread().join();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            scraper.getThread().join();
+        }
+        catch (InterruptedException e) {
+
+        }
+
+        for (HashMap<String, String> line : scraper.scrapedData) {
+            System.out.println(line);
         }
     }
 }
